42452a2bfbcc35899d16fa9f1a40a767f44a3957
Bogdan Sinik <bogdansinik2001@gmail.com>
2023-06-30 17:31:04 +0200

    Working LSL and files that are not needed were removed

--- LSL.m
+++ LSL.m
@@ -35,27 +35,37 @@ while true
     chunk = [chunk,inlet.pull_chunk()];
     % Check if any data is received
     
-    if (length(chunk) >= 2000 )
+    if (length(chunk) >= 500 )
 
         % Extract the channels of interest from the chunk
         eegData = chunk(ChanIndexes,:);
        
         % Preprocess the EEG data
         %preprocessedData = preprocess(eegData);
+        average_reference = mean(eegData, 1);
+        rereferenced_data  = eegData - average_reference;
+        % 
+        % Define filter parameters
+        order = 4;  % Filter order
+        lowpass_cutoff = 50;  % Low-pass cutoff frequency in Hz
+        highpass_cutoff = 1;  % High-pass cutoff frequency in Hz
 
+        % Design the low-pass filter
+        [b_low, a_low] = butter(order, lowpass_cutoff / (samplingRate/2), 'low');
+
+        % Design the high-pass filter
+        [b_high, a_high] = butter(order, highpass_cutoff / (samplingRate/2), 'high');
+
+        % Apply the filters
+        filtered_data = filtfilt(b_low, a_low, transpose(rereferenced_data));
+        filtered_data = filtfilt(b_high, a_high, filtered_data);
         % Perform classification using the model
-        predictedLabels = classify(net, eegData);
-        % sum1 = sum(predictedLabels(1,:));
-        % sum2 = sum(predictedLabels(2,:));
-        % if(sum1 > sum2) disp("EyesClosed"); EC_count =  EC_count+1; end
-        % if(sum1 == sum2) disp("unknown"); end
-        % if(sum1 < sum2) disp("EyesOpen");EO_count =  EO_count+1; end
-        % % Do something with the predicted labels
+        predictedLabels = classify(net,(filtered_data)');
         %disp(predictedLabels);
         majorityCategory = mode(cat(1, predictedLabels));
         disp(majorityCategory);
         chunk = [];
-        
+
     end
     
     % Add a delay or use an event-based approach to control the data acquisition rate
