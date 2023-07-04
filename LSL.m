8c70e7bdd78593a5ffe90e824ced2e5cc8b77bbe
Bogdan Sinik <bogdansinik2001@gmail.com>
2023-07-04 10:24:16 +0200

    Test accuracy 95% and working LSL pushed

--- LSL.m
+++ LSL.m
@@ -24,47 +24,28 @@ channelsToConsider = {'C3','C4','Fp1','Fp2','P7','P8','O1','O2'};
 
 % Define the desired sampling rate
 samplingRate = 500;
-
-% Calculate the number of samples in each chunk based on the desired sampling rate
-chunkSize = ceil(samplingRate);  % Assuming you want 1 second of data in each chunk
-EC_count = 0;
-EO_count = 0;
 chunk = [];
+chunkSize = 500;
+filterChunkLength = 1000;
 while true
     % Retrieve a chunk of EEG data
     chunk = [chunk,inlet.pull_chunk()];
     % Check if any data is received
     
-    if (length(chunk) >= 500 )
-
+    if (length(chunk) >= filterChunkLength )
+        bpFilt = designfilt('bandpassiir','FilterOrder',2, ...
+         'HalfPowerFrequency1',1,'HalfPowerFrequency2',50, ...
+         'SampleRate',srate);
+        filtered_data = (filter(bpFilt,(chunk)'))';
         % Extract the channels of interest from the chunk
-        eegData = chunk(ChanIndexes,:);
-       
-        % Preprocess the EEG data
-        %preprocessedData = preprocess(eegData);
-        average_reference = mean(eegData, 1);
-        rereferenced_data  = eegData - average_reference;
-        % 
-        % Define filter parameters
-        order = 4;  % Filter order
-        lowpass_cutoff = 50;  % Low-pass cutoff frequency in Hz
-        highpass_cutoff = 1;  % High-pass cutoff frequency in Hz
-
-        % Design the low-pass filter
-        [b_low, a_low] = butter(order, lowpass_cutoff / (samplingRate/2), 'low');
-
-        % Design the high-pass filter
-        [b_high, a_high] = butter(order, highpass_cutoff / (samplingRate/2), 'high');
-
-        % Apply the filters
-        filtered_data = filtfilt(b_low, a_low, transpose(rereferenced_data));
-        filtered_data = filtfilt(b_high, a_high, filtered_data);
+        eegData = filtered_data(ChanIndexes,length(chunk)-chunkSize:length(chunk));
         % Perform classification using the model
-        predictedLabels = classify(net,(filtered_data)');
+        predictedLabels = classify(net,eegData);
+        predict(net,eegData)
         %disp(predictedLabels);
         majorityCategory = mode(cat(1, predictedLabels));
         disp(majorityCategory);
-        chunk = [];
+        chunk = chunk(:,length(chunk)-chunkSize:length(chunk));
 
     end
     
--- testDataMaker.m
+++ testDataMaker.m
@@ -1,11 +1,11 @@
-load("DryEEG8ChannelsIndexes.mat")
 eeglab
+load('DryEEG8ChannelsIndexes.mat')
 XTest = {}
 TTest = {}
 srate = 500
 % EEG = pop_loadbv('C:\Users\Bogdan\MATLAB Drive\EEG Recordings\Bogdan\', 'BogdanEC.vhdr'); 
-% EEG = pop_resample( EEG, srate); 
-% EEG = pop_reref( EEG, []);
+% %EEG = pop_resample( EEG, srate); 
+% %EEG = pop_reref( EEG, []);
 % EEG = pop_eegfiltnew(EEG, 'locutoff',1,'hicutoff',50);
 % 
 % XTest{end+1,1} = EEG.data(ChanIndexes,:)
@@ -13,29 +13,33 @@ srate = 500
 % TTest{end+1,1} = categorical( zeros(1,Len), [0 1], {'eyesClosed', 'eyesOpen'});
 % 
 % EEG = pop_loadbv('C:\Users\Bogdan\MATLAB Drive\EEG Recordings\Bogdan\', 'BogdanEO.vhdr'); 
-% EEG = pop_resample( EEG, srate); 
-% EEG = pop_reref( EEG, []);
+% %EEG = pop_resample( EEG, srate); 
+% %EEG = pop_reref( EEG, []);
 % EEG = pop_eegfiltnew(EEG, 'locutoff',1,'hicutoff',50);
 % 
 % XTest{end+1,1} = EEG.data(ChanIndexes,:)
 % Len = length(XTest{end,1})
 % TTest{end+1,1} = categorical( ones(1,Len), [0 1], {'eyesClosed', 'eyesOpen'});
-
 EEG = pop_loadbv('C:\Users\Bogdan\MATLAB Drive\EEG Recordings\Kostas\', 'KostasEC.vhdr'); 
-EEG = pop_resample( EEG, srate); 
-EEG = pop_reref( EEG, []);
-EEG = pop_eegfiltnew(EEG, 'locutoff',1,'hicutoff',50);
-
-XTest{end+1,1} = EEG.data(ChanIndexes,:)
+%EEG = pop_resample( EEG, srate); 
+%EEG = pop_reref( EEG, []);
+%EEG = pop_eegfiltnew(EEG, 'locutoff',1,'hicutoff',50);
+bpFilt = designfilt('bandpassiir','FilterOrder',2, ...
+         'HalfPowerFrequency1',1,'HalfPowerFrequency2',50, ...
+         'SampleRate',srate);
+outData = (filter(bpFilt,(EEG.data)'))';
+XTest{end+1,1} = outData(ChanIndexes,:)
+%XTest{end+1,1} = EEG.data(ChanIndexes,:)
 Len = length(XTest{end,1})
 TTest{end+1,1} = categorical( zeros(1,Len), [0 1], {'eyesClosed', 'eyesOpen'});
-
 EEG = pop_loadbv('C:\Users\Bogdan\MATLAB Drive\EEG Recordings\Kostas\', 'KostasEO.vhdr'); 
-EEG = pop_resample( EEG, srate); 
-EEG = pop_reref( EEG, []);
-EEG = pop_eegfiltnew(EEG, 'locutoff',1,'hicutoff',50);
+%EEG = pop_resample( EEG, srate); 
+%EEG = pop_reref( EEG, []);
 
-XTest{end+1,1} = EEG.data(ChanIndexes,:)
+outData = (filter(bpFilt,(EEG.data)'))';
+XTest{end+1,1} = outData(ChanIndexes,:)
+%EEG = pop_eegfiltnew(EEG, 'locutoff',1,'hicutoff',50);
+
+%XTest{end+1,1} = EEG.data(ChanIndexes,:)
 Len = length(XTest{end,1})
 TTest{end+1,1} = categorical( ones(1,Len), [0 1], {'eyesClosed', 'eyesOpen'});
-
